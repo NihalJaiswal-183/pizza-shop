40d89419ceb738b9aee82d6636b4ca2217ad467d
Nihal <nihaljai2000@gmail.com>
2021-12-27 17:57:09 +0530

    add home page

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express =require("express");
 const ejs =require("ejs");
-// const expresslayouts=require("express-ejs-layouts");
+const expresslayout=require("express-ejs-layouts");
 const app=express();
 const path=require("path");
 const PORT=process.env.PORT || 3000;
 app.set("views",path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+app.use(expresslayout);
 app.use(express.static('public'));
 app.get("/",(req,res)=>{
     res.render('home');
+})
+app.get("/cart",(req,res)=>{
+    res.render('customer/cart');
+})
+app.get("/login",(req,res)=>{
+    res.render('auth/login');
+})
+app.get("/register",(req,res)=>{
+    res.render('auth/register');
 })
 app.listen(PORT,()=>{
     console.log("app is running at port "+PORT)
